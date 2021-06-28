821d708f5f6862f9a7b88c6050caf020a38afc8c
masyo-lab <masyo.lab@gmail.com>
2021-06-28 22:48:53 +0900

    ビルド対象を変更

--- Assets/Editor/CI/Builder.cs
+++ Assets/Editor/CI/Builder.cs
@@ -1,11 +1,8 @@
 using UnityEditor;
 
-namespace CI
-{
-    public static class Builder 
-    {
-        public static void Build()
-        {
+namespace CI {
+    public static class Builder {
+        public static void Build() {
             AssetDatabase.ExportPackage("Assets/FavoritesAsset", "FavoritesAsset.unitypackage");
         }
     }
