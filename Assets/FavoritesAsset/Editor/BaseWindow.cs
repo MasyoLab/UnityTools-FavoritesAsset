04fcd28966e1a78078dafa1024e142c15a0cf4bb
masyo-lab <masyo.lab@gmail.com>
2021-07-01 00:47:14 +0900

    【Window】 ・WinSys

---
+++ Assets/FavoritesAsset/Editor/BaseWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MasyoLab.Editor.FavoritesAsset {
+
+    abstract class BaseWindow {
+        protected FavoritesManager _manager = null;
+
+        public abstract void OnGUI();
+        public virtual void SetFavoritesManager(FavoritesManager manager) {
+            _manager = manager;
+        }
+    }
+}
---
+++ Assets/FavoritesAsset/Editor/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace MasyoLab.Editor.FavoritesAsset {
+
+    public class MainWindow : EditorWindow {
+        List<BaseWindow> _windows = new List<BaseWindow>();
+
+        _Ty GetWindowClass<_Ty>() where _Ty : BaseWindow, new() {
+            foreach (var item in _windows) {
+                _Ty win = item as _Ty;
+                if (win == null)
+                    continue;
+                return win;
+            }
+
+            var newWin = new _Ty();
+            _windows.Add(newWin);
+            return newWin;
+        }
+    }
+}
