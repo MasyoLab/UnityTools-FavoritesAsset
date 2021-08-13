1ce627045f48e18ee06b214ee17105d6a442a528
masyo-lab <masyo.lab@gmail.com>
2021-08-13 12:37:01 +0900

    add data class

---
+++ Assets/MasyoLab/FavoritesAsset/Editor/Data/GroupData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+//=========================================================
+//
+//  developer : MasyoLab
+//  github    : https://github.com/MasyoLab/UnityTools-FavoritesAsset
+//
+//=========================================================
+
+namespace MasyoLab.Editor.FavoritesAsset {
+
+    [System.Serializable]
+    class GroupData {
+        public string SelectGroup = string.Empty;
+        public List<string> GroupList = null;
+    }
+}
