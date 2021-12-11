9420aa6ff96462964eba8a077d1f7db61def459f
masyo-lab <masyo.lab@gmail.com>
2021-12-11 16:58:44 +0900

    ドラッグ更新、不具合あり

--- Assets/MasyoLab/FavoritesAsset/Editor/Manager/DragManager.cs
+++ Assets/MasyoLab/FavoritesAsset/Editor/Manager/DragManager.cs
@@ -5,9 +5,12 @@ using UnityEngine.UIElements;
 namespace MasyoLab.Editor.FavoritesAsset {
     class DragManager : BaseManager {
 
-        private bool _isDragPerformed = false;
-        private bool _isDragging = false;
         private bool _isInitialized = false;
+        private bool _isDragPerformed = false;
+        private bool _isDrag = false;
+        private bool _isMove = false;
+        private bool _onClick = false;
+        AssetData _assetData = null;
 
         public DragManager(IPipeline pipeline) : base(pipeline) { }
         ~DragManager() {
@@ -22,51 +25,136 @@ namespace MasyoLab.Editor.FavoritesAsset {
             var element = new VisualElement();
             element.style.flexGrow = 1f;
 
-            element.RegisterCallback<MouseDownEvent>(OnMouseDownEvent);
-            element.RegisterCallback<MouseUpEvent>(OnMouseUpEvent);
+            element.RegisterCallback<PointerDownEvent>(OnPointerDownEvent);
+            element.RegisterCallback<PointerUpEvent>(OnPointerUpEvent);
+
+            element.RegisterCallback<PointerOutEvent>(OnPointerOutEvent);
+            element.RegisterCallback<PointerOverEvent>(OnPointerOverEvent);
+
             element.RegisterCallback<DragUpdatedEvent>(OnDragUpdatedEvent);
+            element.RegisterCallback<PointerMoveEvent>(OnPointerMoveEvent);
 
             _pipeline.Root.rootVisualElement.Add(element);
             SceneView.duringSceneGui += sv => OnDragEnd();
             EditorApplication.hierarchyWindowItemOnGUI += (id, rect) => OnDragEnd();
+            EditorApplication.projectWindowItemOnGUI += (id, rect) => OnDragEnd();
         }
 
-        void OnMouseDownEvent(MouseDownEvent evt) {
-            //DragAndDrop.PrepareStartDrag();
-            //DragAndDrop.StartDrag("Dragging");
-            //Selection.activeGameObject = null;
-            Debug.Log("MouseDownEvent!!");
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        void ClearEvent() {
+            _onClick = false;
+            _isDrag = false;
+            _isMove = false;
+            _assetData = null;
+            DragAndDrop.AcceptDrag();
+            DragAndDrop.activeControlID = 0;
+            Event.current.Use();
         }
 
-        void OnMouseUpEvent(MouseUpEvent evt) {
-            Debug.Log("MouseUpEvent!!");
+        /// <summary>
+        /// クリック時に実行
+        /// </summary>
+        /// <param name="evt"></param>
+        void OnPointerDownEvent(PointerDownEvent evt) {
+            Debug.Log(Event.current.type);
+
+            _onClick = true;
+            _isDrag = false;
+            _isMove = false;
+        }
+
+        /// <summary>
+        /// エディタ内でクリックが完了した
+        /// </summary>
+        /// <param name="evt"></param>
+        void OnPointerUpEvent(PointerUpEvent evt) {
+            ClearEvent();
         }
 
         void OnDragUpdatedEvent(DragUpdatedEvent evt) {
-            //DragAndDrop.visualMode = DragAndDropVisualMode.Move;
-            Debug.Log("DragUpdatedEvent!!");
+            DragAndDrop.visualMode = DragAndDropVisualMode.Move;
+        }
+
+        /// <summary>
+        /// ポインターが動いた際に実行
+        /// </summary>
+        /// <param name="evt"></param>
+        void OnPointerMoveEvent(PointerMoveEvent evt) {
+            // クリックしていない
+            if (!_onClick)
+                return;
+            _isMove = true;
+        }
+
+        /// <summary>
+        /// エディタから外れた際に実行
+        /// </summary>
+        /// <param name="evt"></param>
+        void OnPointerOutEvent(PointerOutEvent evt) {
+            // クリックしていない
+            if (!_onClick)
+                return;
+            if (Event.current.type != EventType.MouseDrag)
+                return;
+            if (_assetData == null)
+                return;
+
+            DragAndDrop.PrepareStartDrag();
+            DragAndDrop.StartDrag("Dragging");
+            DragAndDrop.objectReferences = new Object[] { _assetData.GetObject() };
+            _isDrag = true;
+        }
+
+        /// <summary>
+        /// エディタに触れた際に実行
+        /// </summary>
+        /// <param name="evt"></param>
+        void OnPointerOverEvent(PointerOverEvent evt) {
+            // クリックしていない,ドラッグ中ではない
+            if (!_onClick || !_isDrag)
+                return;
+
+            ClearEvent();
         }
 
         private void OnDragEnd() {
 
+            // 対象にドラッグ
             if (Event.current.type == EventType.DragPerform) {
                 _isDragPerformed = true;
             }
 
+            // ドラッグ完了
             if (Event.current.type == EventType.DragExited) {
-                if (_isDragging && _isDragPerformed) {
-                    _isDragging = false;
+                if (_isDragPerformed) {
                     _isDragPerformed = false;
-
-                    var go = Selection.activeGameObject;
-                    // Do your **OnDragEnd callback on go** here
+                    ClearEvent();
                 }
+            }
+        }
+
+        public void OnMouseDownEvent(Rect rect, int index) {
+            if (_isMove)
+                return;
+            if (_assetData != null)
+                return;
+            if (Event.current == null)
+                return;
+
+            if (Event.current.isMouse && Event.current.button == 0 && rect.Contains(Event.current.mousePosition)) {
+                _assetData = _pipeline.Favorites.Data[index];
+                DragAndDrop.activeControlID = 0;
+                Event.current.Use();
+                DragAndDrop.visualMode = DragAndDropVisualMode.Move;
             }
         }
 
         private void OnDestroy() {
             SceneView.duringSceneGui -= sv => OnDragEnd();
             EditorApplication.hierarchyWindowItemOnGUI -= (id, rect) => OnDragEnd();
+            EditorApplication.projectWindowItemOnGUI -= (id, rect) => OnDragEnd();
         }
     }
 }
