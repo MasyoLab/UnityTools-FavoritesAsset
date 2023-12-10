f31e57e4bccc4826b742900ba72ea63731b07f1d
MasyoLab <masyo.lab@gmail.com>
2023-12-10 19:34:52 +0900

    拡張サンプル

---
+++ Assets/Editor/Examples/RainbowFolders/FavoritesAssetWindowExample.cs
@@ -0,0 +1,35 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using MasyoLab.Editor;
+using Borodar.RainbowFolders.Editor.Settings;
+
+/// <summary>
+/// Compatibility with RainbowFolders in the Favorites Assets window.
+/// </summary>
+[InitializeOnLoad]
+public class FavoritesAssetWindowRainbowFoldersSupportExample
+{
+    static FavoritesAssetWindowRainbowFoldersSupportExample()
+    {
+        FavoritesAssetWindow.GetFavoritesAssetIcon += GetAssetIcon;
+    }
+
+    private static Texture GetAssetIcon(string folderPath)
+    {
+        var setting = RainbowFoldersSettings.Instance;
+        if (setting == null)
+        {
+            return null;
+        }
+        var texture = setting.GetFolderIcon(folderPath, true);
+        if (texture == null)
+        {
+            return null;
+        }
+        return texture;
+    }
+}
+*/
